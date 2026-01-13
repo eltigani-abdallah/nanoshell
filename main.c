1963a0777ef5dcca1033ff59b899951bb0ee63fe
eltigani-abdallah <eltigani.abdallah@laplateforme.io>
2026-01-13 14:40:36 +0100

    docs: add comments to main.c

--- main.c
+++ main.c
@@ -6,6 +6,7 @@
 #include "./include/ls.h"
 #include "./include/manager.h"
 
+//buffer size in a macro to simplify access
 #define INPUT_BUFFER_SIZE (1024 * 4)
 
 
@@ -14,26 +15,35 @@
 
 
 int main() {
-
+    //condition for the while loop
     int running = 1;
+    //process id for the execv process
     int pid = -1;
 
     while (running) {
+        //buffer to replace with user input
         char line[INPUT_BUFFER_SIZE]={0};
 
 
 
-
+        //prompt
         printf("Hello→ ");
+
+        // poplate line from standard input (stdin)
         fgets(line, INPUT_BUFFER_SIZE,stdin);
+
+        // fgets receives text with \n at the end. ↓ replaces \n with \0 to help in comparisons
         line[strcspn(line, "\n") ]= '\0';
 
+        // if the user asks for exit, turns running to 0 and exits the big loop
+        // necessary to do before the fork (built in)
         if (strcmp(line, "exit")==0) {
             running = 0;
         }
 
-
+        // create a new process to maintain functionality
         pid = fork();
+
 
         if (pid == 0) {
 
