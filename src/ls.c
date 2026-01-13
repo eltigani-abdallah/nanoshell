19a74b76deb93ef4d388248b5b910e9f11c1e03b
eltigani-abdallah <eltigani.abdallah@laplateforme.io>
2026-01-13 14:42:45 +0100

    docs: add comments to ls.c

--- src/ls.c
+++ src/ls.c
@@ -1,9 +1,11 @@
 #include "../include/ls.h"
 
 void ls() {
+    //arguments to be passed in to execve function
     char* argv[]={"ls", NULL};
     char* envp[]={NULL};
 
+    //execve("path to exectable", arguments, envp)
     execve("/bin/ls",argv, envp);
 
 }
